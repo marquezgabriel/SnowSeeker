0ef0c9601aaac1fb32fbdd9f9252e5e645c1f685
Gabriel Marquez <gabrielmarquezrivas@gmail.com>
2022-09-09 09:43:28 -0400

    Initial Commit

---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Gabriel Marquez on 2022-09-09.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
