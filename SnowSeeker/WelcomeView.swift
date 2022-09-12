3553a46f75d6e0abce59260db50eb4e92d462ea0
Gabriel Marquez <gabrielmarquezrivas@gmail.com>
2022-09-12 10:02:35 -0400

    Day 97 - Building a primary list of items

---
+++ SnowSeeker/WelcomeView.swift
@@ -0,0 +1,21 @@
+//
+//  WelcomeView.swift
+//  SnowSeeker
+//
+//  Created by Gabriel Marquez on 2022-09-12.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        Text("Welcome to SnowSeeker!")
+            .font(.largeTitle)
+    }
+}
+
+struct WelcomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        WelcomeView()
+    }
+}
