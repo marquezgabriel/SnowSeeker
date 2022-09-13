2648ad7c422284ead43c4362db75ff002f9f62fa
Gabriel Marquez <gabrielmarquezrivas@gmail.com>
2022-09-13 10:15:35 -0400

    Day 98 - Changing a view’s layout in response to size classes

--- SnowSeeker/ResortView.swift
+++ SnowSeeker/ResortView.swift
@@ -9,6 +9,10 @@ import SwiftUI
 
 struct ResortView: View {
     let resort: Resort
+    
+    @Environment(\.horizontalSizeClass) var sizeClass
+    @Environment(\.dynamicTypeSize) var typeSize
+
 
     var body: some View {
         ScrollView {
@@ -18,11 +22,17 @@ struct ResortView: View {
                     .scaledToFit()
                 
                 HStack {
-                    ResortDetailsView(resort: resort)
-                    SkiDetailsView(resort: resort)
+                    if sizeClass == .compact && typeSize > .large {
+                        VStack(spacing: 10) { ResortDetailsView(resort: resort) }
+                        VStack(spacing: 10) { SkiDetailsView(resort: resort) }
+                    } else {
+                        ResortDetailsView(resort: resort)
+                        SkiDetailsView(resort: resort)
+                    }
                 }
                 .padding(.vertical)
                 .background(Color.primary.opacity(0.1))
+                .dynamicTypeSize(...DynamicTypeSize.xxxLarge)
 
                 Group {
                     Text(resort.description)
